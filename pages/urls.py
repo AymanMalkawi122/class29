fe1ce8936409329ce48ab05624af0dc6fa63d10d
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-07-22 21:36:48 +0300

    add tasks link to navbar

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from .views import HomePageView, AboutPageView
 
+from tasks.views import TaskListView
+
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("tasks/", TaskListView.as_view(), name="task_list")
 ]
